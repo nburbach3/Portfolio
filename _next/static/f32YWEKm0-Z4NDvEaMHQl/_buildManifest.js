b9003a98f6759679bf8202f907aff0bcc69a6ff1
Nick Burbach <nburbach@huskers.unl.edu>
2021-10-28 13:47:41 -0500

    Updates

---
+++ _next/static/f32YWEKm0-Z4NDvEaMHQl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/70b165ca-3457dda79fd5a121.js","static/chunks/d7eeaac4-0128fb2e70faded6.js","static/chunks/pages/index-ab03ef0cf5be66a8.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
